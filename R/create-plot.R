132f0a68611d189ca32a419b78fd8acfce8b0a75
junyasuzuki23 <junya.suzuki@tokiomarinekiln.com>
2025-06-09 14:32:18 +0100

    colour2

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(aes = "green")
+  geom_point(aes = "purple")
 
 time_series =
   df %>%
