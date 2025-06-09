893449e3c10b81cf85d617b6ad5c8c7dfd633a83
junyasuzuki23 <junya.suzuki@tokiomarinekiln.com>
2025-06-09 10:28:55 +0100

    time

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,11 +16,15 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point()
-+
 
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = time_series)
+  
+  
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
-  aa 
-  aaa
-  df 
-   aa
